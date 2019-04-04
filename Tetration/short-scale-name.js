62bcc4c25f494a0eafa3317ebb27f285847a2d76
hyp cos <48753701+hypcos@users.noreply.github.com>
2019-04-05 00:05:24 +0800

    Use arrow

--- Tetration/short-scale-name.js
+++ Tetration/short-scale-name.js
@@ -2,15 +2,15 @@
 //Standard -illion seires go up to centillion
 const _tier0=['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
 ,_ty0=['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
-,ConvertTier0 = function(n){// 1 <= n <= 999
+,ConvertTier0 = n=>{// 1 <= n <= 999
    var d21=n%100;
    return (n>=100?_tier0[Math.floor(n/100)-1]+' hundred'+(d21?' and ':''):'')+(d21?d21<20?_tier0[d21-1]:_ty0[Math.floor(d21/10)-2]+(n%10?'-'+_tier0[n%10-1]:''):'')
 }
 ,_tier1f=['thousand','million','billion','trillion','quadrillion','quintillion','sextillion','septillion','octillion','nonillion']
 ,_tier1=['','un','duo','tre','quattuor','quin','sex','septen','octo','novem']
 ,_ty1f=['decillion','vigintillion','trigintillion','quadragintillion','quinquagintillion','sexagintillion','septuagintillion','octogintillion','nonagintillion']
-,ConvertStdIllion = function(n){return n<100?n>=10?_tier1[n%10]+_ty1f[Math.floor(n/10)-1]:_tier1f[n]:n==100?'centillion':'('+n+')illion'}// 0 <= n <= 100
-,ShortScaleName = function(x){
+,ConvertStdIllion = n=> n<100?n>=10?_tier1[n%10]+_ty1f[Math.floor(n/10)-1]:_tier1f[n]:n==100?'centillion':'('+n+')illion'// 0 <= n <= 100
+,ShortScaleName = x=>{
    var nx = Natural(x),inp,str='',fl,expo;
    if(nx===0) return 'zero';
    if(LessQ(nx,{neg:false,recip:false,val:230258509299.4044,pt:1})){
@@ -42,4 +42,5 @@ const _tier0=['one','two','three','four','five','six','seven','eight','nine','te
       return str.endsWith(', ')?str.slice(0,-2):str
    }
    return DisplayLongInt(nx);
-};
+}
+,UpperFirst = x=>x[0].toUpperCase()+x.substr(1)
