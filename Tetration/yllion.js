5e7e2a2be9d9be7157a25caf5f711447d50336a7
hyp cos <48753701+hypcos@users.noreply.github.com>
2019-05-08 10:45:36 +0800

    Update yllion.js

--- Tetration/yllion.js
+++ Tetration/yllion.js
@@ -1,2 +1,2 @@
 //"Standard" -yllion series go up to centyllion
-const ConvertStdYllion = n=> n<3?['ten','hundred','myriad'][n]:ConvertStdIllion(n-2).replace('illion','yllion')//name of 10^2^n
+const ConvertStdYllion = n=> n<3?['ten','hundred','myriad'][n]:ConvertStdIllion(n-2).slice(0,-6).concat('yllion')//name of 10^2^n
