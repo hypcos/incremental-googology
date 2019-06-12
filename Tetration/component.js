2a32f152c6b69b72ce41c03050bcfe25cff000e7
hyp cos <48753701+hypcos@users.noreply.github.com>
2019-06-12 09:08:33 +0800

    Update component.js

--- Tetration/component.js
+++ Tetration/component.js
@@ -5,7 +5,7 @@ const Pointer = a=>{
    return [pointer,a[l]]
 };
 Vue.component('buy',{
-   props:['info','amount','bought','mult']
+   props:['info','amount','bought','mult','textsize']
    ,computed:{
       Amount(){return Pointer(this.amount)}
       ,Bought(){return Pointer(this.bought)}
@@ -22,64 +22,72 @@ Vue.component('buy',{
    }
    ,methods:{
       Buy(){
-         if(this.Costo[0]===v) v[this.Costo[1]]=Minus(v[this.Costo[1]],this.Cost);
-         else Vue.set(this.Costo[0],this.Costo[1],Minus(this.Costo[0][this.Costo[1]],this.Cost));
-         Vue.set(this.Amount[0],this.Amount[1],Natural(Plus(this.Amount[0][this.Amount[1]],1)));
-         Vue.set(this.Bought[0],this.Bought[1],Natural(Plus(this.Bought[0][this.Bought[1]],1)))
+         const amount=this.Amount,bought=this.Bought,costo=this.Costo;
+         if(costo[0]===v) v[costo[1]]=Minus(v[costo[1]],this.Cost);
+         else Vue.set(costo[0],costo[1],Minus(costo[0][costo[1]],this.Cost));
+         Vue.set(amount[0],amount[1],Natural(Plus(amount[0][amount[1]],1)));
+         Vue.set(bought[0],bought[1],Natural(Plus(bought[0][bought[1]],1)))
       }
       ,BuyMax(){
-         var delta=Floor(Minus(this.info.solve(Plus(this.Costo[0][this.Costo[1]],this.info.sum(this.Bought[0][this.Bought[1]]))),this.Bought[0][this.Bought[1]]));
+         const amount=this.Amount,bought=this.Bought,costo=this.Costo,infosum=this.info.sum;
+         var delta=Floor(Minus(this.info.solve(Plus(costo[0][costo[1]],infosum(bought[0][bought[1]]))),bought[0][bought[1]]));
          if(Sign(delta)<0) delta=0;
-         if(this.Costo===v){
-            v[this.Costo[1]]=Minus(v[this.Costo[1]],Minus(this.info.sum(Plus(this.Bought[0][this.Bought[1]],delta)),this.info.sum(this.Bought[0][this.Bought[1]])));
-            if(Sign(v[this.Costo[1]])<0) v[this.Costo[1]]=0;
+         if(costo===v){
+            v[costo[1]]=Minus(v[costo[1]],Minus(infosum(Plus(bought[0][bought[1]],delta)),infosum(bought[0][bought[1]])));
+            if(Sign(v[costo[1]])<0) v[costo[1]]=0;
          }else{
-            Vue.set(this.Costo[0],this.Costo[1],Minus(this.Costo[0][this.Costo[1]],Minus(this.info.sum(Plus(this.Bought[0][this.Bought[1]],delta)),this.info.sum(this.Bought[0][this.Bought[1]]))))
-            if(Sign(this.Costo[0][this.Costo[1]])<0) Vue.set(this.Costo[0],this.Costo[1],0);
+            Vue.set(costo[0],costo[1],Minus(costo[0][costo[1]],Minus(infosum(Plus(bought[0][bought[1]],delta)),infosum(bought[0][bought[1]]))))
+            if(Sign(costo[0][costo[1]])<0) Vue.set(costo[0],costo[1],0);
          }
-         Vue.set(this.Amount[0],this.Amount[1],Natural(Plus(this.Amount[0][this.Amount[1]],delta)));
-         Vue.set(this.Bought[0],this.Bought[1],Natural(Plus(this.Bought[0][this.Bought[1]],delta)))
+         Vue.set(amount[0],amount[1],Natural(Plus(amount[0][amount[1]],delta)));
+         Vue.set(bought[0],bought[1],Natural(Plus(bought[0][bought[1]],delta)))
       }
    }
    ,template:'<button class="cell" :disabled="Cant" @mousedown.stop="Buy()" @dblclick.stop="BuyMax()" :title="info.tooltip">\
-      <span class="strong" v-show="info.text">{{info.text}}<br></span>\
-      <span class="strong" v-show="Content">{{Content}}<br></span>\
+      <b :style="{\'font-size\':textsize+\'px\'}" v-show="info.text">{{info.text}}<br></b>\
+      <b :style="{\'font-size\':textsize+\'px\'}" v-show="Content">{{Content}}<br></b>\
       {{ShowAmount}}<br>\
       {{ShowMult}}×<br>\
       Cost: {{ShowCost}}\
    <slot></slot></button>'
 });
 Vue.component('bulk',{
-   props:['info','bulkinfo','amount','bought']
+   props:['bulkinfo']
    ,computed:{
-      Amount(){return Pointer(this.amount)}
-      ,Bought(){return Pointer(this.bought)}
-      ,Costo(){return Pointer(this.info.costo)}
-      ,Cost(){return Minus(this.info.sum(Times(Plus(Floor(Divide(this.Bought[0][this.Bought[1]],this.bulkinfo.number)),1),this.bulkinfo.number)),this.info.sum(this.Bought[0][this.Bought[1]]))}
-      ,Cant(){return LessQ(this.Costo[0][this.Costo[1]],this.Cost)}
+      Cost(){
+         var parentsum=this.$parent.info.sum,bought=this.$parent.Bought;
+         bought=bought[0][bought[1]];
+         return Minus(parentsum(Times(Plus(Floor(Divide(bought,this.bulkinfo.number)),1),this.bulkinfo.number)),parentsum(bought))
+      }
+      ,Cant(){
+         const costo=this.$parent.Costo;
+         return LessQ(costo[0][costo[1]],this.Cost)
+      }
    }
    ,methods:{
       Buy(){
-         if(this.Costo[0]===v) v[this.Costo[1]]=Minus(v[this.Costo[1]],this.Cost);
-         else Vue.set(this.Costo[0],this.Costo[1],Minus(this.Costo[0][this.Costo[1]],this.Cost));
-         var delta=Divide(this.Bought[0][this.Bought[1]],this.bulkinfo.number);
+         const amount=this.$parent.Amount,bought=this.$parent.Bought,costo=this.$parent.Costo;
+         if(costo[0]===v) v[costo[1]]=Minus(v[costo[1]],this.Cost);
+         else Vue.set(costo[0],costo[1],Minus(costo[0][costo[1]],this.Cost));
+         var delta=Divide(bought[0][bought[1]],this.bulkinfo.number);
          delta=Times(Minus(Plus(Floor(delta),1),delta),this.bulkinfo.number);
-         Vue.set(this.Amount[0],this.Amount[1],Natural(Plus(this.Amount[0][this.Amount[1]],delta)));
-         Vue.set(this.Bought[0],this.Bought[1],Natural(Plus(this.Bought[0][this.Bought[1]],delta)))
+         Vue.set(amount[0],amount[1],Natural(Plus(amount[0][amount[1]],delta)));
+         Vue.set(bought[0],bought[1],Natural(Plus(bought[0][bought[1]],delta)))
       }
       ,BuyMax(){
-         var delta=Minus(Times(Floor(Divide(this.info.solve(Plus(this.Costo[0][this.Costo[1]],this.info.sum(this.Bought[0][this.Bought[1]])))
-            ,this.bulkinfo.number)),this.bulkinfo.number),this.Bought[0][this.Bought[1]]);
+         const amount=this.$parent.Amount,bought=this.$parent.Bought,costo=this.$parent.Costo,parentsum=this.$parent.info.sum;
+         var delta=Minus(Times(Floor(Divide(this.$parent.info.solve(Plus(costo[0][costo[1]],parentsum(bought[0][bought[1]])))
+            ,this.bulkinfo.number)),this.bulkinfo.number),bought[0][bought[1]]);
          if(Sign(delta)<0) delta=0;
-         if(this.Costo===v){
-            v[this.Costo[1]]=Minus(v[this.Costo[1]],Minus(this.info.sum(Plus(this.Bought[0][this.Bought[1]],delta)),this.info.sum(this.Bought[0][this.Bought[1]])));
-            if(Sign(v[this.Costo[1]])<0) v[this.Costo[1]]=0;
+         if(costo===v){
+            v[costo[1]]=Minus(v[costo[1]],Minus(parentsum(Plus(bought[0][bought[1]],delta)),parentsum(bought[0][bought[1]])));
+            if(Sign(v[costo[1]])<0) v[costo[1]]=0;
          }else{
-            Vue.set(this.Costo[0],this.Costo[1],Minus(this.Costo[0][this.Costo[1]],Minus(this.info.sum(Plus(this.Bought[0][this.Bought[1]],delta)),this.info.sum(this.Bought[0][this.Bought[1]]))))
-            if(Sign(this.Costo[0][this.Costo[1]])<0) Vue.set(this.Costo[0],this.Costo[1],0);
+            Vue.set(costo[0],costo[1],Minus(costo[0][costo[1]],Minus(parentsum(Plus(bought[0][bought[1]],delta)),parentsum(bought[0][bought[1]]))))
+            if(Sign(costo[0][costo[1]])<0) Vue.set(costo[0],costo[1],0);
          }
-         Vue.set(this.Amount[0],this.Amount[1],Natural(Plus(this.Amount[0][this.Amount[1]],delta)));
-         Vue.set(this.Bought[0],this.Bought[1],Natural(Plus(this.Bought[0][this.Bought[1]],delta)))
+         Vue.set(amount[0],amount[1],Natural(Plus(amount[0][amount[1]],delta)));
+         Vue.set(bought[0],bought[1],Natural(Plus(bought[0][bought[1]],delta)))
       }
    }
    ,template:'<button :disabled="Cant" @mousedown.stop="Buy()" @dblclick.stop="BuyMax()">{{bulkinfo.text}}</button>'
